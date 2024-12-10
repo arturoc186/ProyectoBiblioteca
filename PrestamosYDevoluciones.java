a8783930a04b35fcbc541427c1d02dc549102150
Victor-036 <victorvzquez036@gmail.com>
2024-12-10 13:14:46 +0100

    Update PrestamosYDevoluciones.java

--- PrestamosYDevoluciones.java
+++ PrestamosYDevoluciones.java
@@ -1,5 +1,15 @@
 public class PrestamosYDevoluciones {
 
+    public static boolean esDisponible(int indiceLibro) {
+        return disponibilidad[indiceLibro];
+    }
+
+    public static void cambiarDisponibilidad(int indiceLibro, boolean estado) {
+        disponibilidad[indiceLibro] = estado;
+        estados[indiceLibro] = estado ? "Disponible" : "Prestado";
+    }
+}
+
     public static void registrarPrestamo(int indiceLibro) {
         if (indiceLibro >= 0 && indiceLibro < ListadoLibros.cantidadLibros) {
             if (ListadoLibros.esDisponible(indiceLibro)) {
